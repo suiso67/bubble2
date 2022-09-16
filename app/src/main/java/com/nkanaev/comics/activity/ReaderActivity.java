87ba881b81eaeec11c86bb4c3840039426a9cd8f
ede <edgar.soldin@web.de>
2022-09-16 20:24:13 +0200

    upgrade to package androidx

--- app/src/main/java/com/nkanaev/comics/activity/ReaderActivity.java
+++ app/src/main/java/com/nkanaev/comics/activity/ReaderActivity.java
@@ -2,10 +2,10 @@ package com.nkanaev.comics.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.view.MenuItem;
 import com.nkanaev.comics.R;
 import com.nkanaev.comics.fragment.ReaderFragment;
