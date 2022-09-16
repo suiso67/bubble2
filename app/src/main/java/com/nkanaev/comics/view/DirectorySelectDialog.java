bcfd049a0b271dfa243ba7cfd4da1e346eedd367
ede <edgar.soldin@web.de>
2022-09-16 20:37:47 +0200

    upgrade to package androidx

--- app/src/main/java/com/nkanaev/comics/view/DirectorySelectDialog.java
+++ app/src/main/java/com/nkanaev/comics/view/DirectorySelectDialog.java
@@ -1,7 +1,7 @@
 package com.nkanaev.comics.view;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatDialog;
+import androidx.appcompat.app.AppCompatDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
