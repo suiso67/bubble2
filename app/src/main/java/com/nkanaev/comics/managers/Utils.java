29a7b0aeefbdfb504ed3c2406402999ac2defcee
ed <edgar.soldin@web.de>
2022-10-19 17:37:56 +0200

    several utility functions added
    API, file name comparators
    cache folder creation
    recursive deletion
    silent close of Closeables

--- app/src/main/java/com/nkanaev/comics/managers/Utils.java
+++ app/src/main/java/com/nkanaev/comics/managers/Utils.java
@@ -1,15 +1,19 @@
 package com.nkanaev.comics.managers;
 
 import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Build;
-import android.content.Context;
-import android.content.pm.ApplicationInfo;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import com.gemalto.jp2.JP2Decoder;
+import com.nkanaev.comics.MainApplication;
 
 import java.io.*;
 import java.security.MessageDigest;
+import java.util.UUID;
 
 import static android.content.Context.ACTIVITY_SERVICE;
 
@@ -44,8 +48,22 @@ public final class Utils {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
     }
 
+    // FileChannel
+    public static boolean isNougatOrLater() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.N;
+    }
+
+    // java.nio.path
+    public static boolean isOreoOrLater() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.O;
+    }
+
+    public static boolean isROrLater() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.R;
+    }
+
     public static int getHeapSize(Context context) {
-        ActivityManager am = (ActivityManager)context.getSystemService(Context.ACTIVITY_SERVICE);
+        ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
         boolean isLargeHeap = (context.getApplicationInfo().flags & ApplicationInfo.FLAG_LARGE_HEAP) != 0;
         int memoryClass = am.getMemoryClass();
         if (isLargeHeap && Utils.isHoneycombOrLater()) {
@@ -58,15 +76,23 @@ public final class Utils {
         int sizeInBytes;
         if (Utils.isHoneycombMR1orLater()) {
             sizeInBytes = bitmap.getByteCount();
-        }
-        else {
+        } else {
             sizeInBytes = bitmap.getRowBytes() * bitmap.getHeight();
         }
         return sizeInBytes / 1024;
     }
 
     public static boolean isImage(String filename) {
-        return filename.toLowerCase().matches(".*\\.(jpg|jpeg|bmp|gif|png|webp)$");
+        return filename.matches("(?i).*\\.(jpg|jpeg|bmp|gif|png|webp|jp2|j2k)$");
+    }
+
+    public static boolean isJP2Stream(BufferedInputStream bis) throws IOException {
+        bis.mark(24);
+        // TODO: JP2Decoder may need only 12bytes looks like, read 24 anyway
+        byte[] b = new byte[24];
+        bis.read(b);
+        bis.reset();
+        return JP2Decoder.isJPEG2000(b);
     }
 
     public static boolean isZip(String filename) {
@@ -78,7 +104,27 @@ public final class Utils {
     }
 
     public static boolean isTarball(String filename) {
-        return filename.toLowerCase().matches(".*\\.(cbt)$");
+        return filename.matches("(?i).*\\.(cbt|tar)$") || isTGZ(filename) || isTBZ(filename) || isTXZ(filename) || isTZST(filename) || isTBR(filename);
+    }
+
+    public static boolean isTGZ(String filename) {
+        return filename.toLowerCase().matches("(?i).*\\.(tar\\.gz|tgz)$");
+    }
+
+    public static boolean isTBZ(String filename) {
+        return filename.toLowerCase().matches("(?i).*\\.(tar\\.bz2?|tbz2?)$");
+    }
+
+    public static boolean isTXZ(String filename) {
+        return filename.toLowerCase().matches("(?i).*\\.(tar\\.xz|txz)$");
+    }
+
+    public static boolean isTZST(String filename) {
+        return filename.toLowerCase().matches("(?i).*\\.(tar\\.zstd?|tzs(|t|td))$");
+    }
+
+    public static boolean isTBR(String filename) {
+        return filename.toLowerCase().matches("(?i).*\\.(tar\\.br|tbr)$");
     }
 
     public static boolean isSevenZ(String filename) {
@@ -91,12 +137,14 @@ public final class Utils {
 
     public static int getDeviceWidth(Context context) {
         DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
-        return Math.round(displayMetrics.widthPixels / displayMetrics.density);
+        int value = Math.round(displayMetrics.widthPixels / displayMetrics.density);
+        return value;
     }
 
     public static int getDeviceHeight(Context context) {
         DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
-        return Math.round(displayMetrics.heightPixels / displayMetrics.density);
+        int value = Math.round(displayMetrics.heightPixels / displayMetrics.density);
+        return value;
     }
 
     public static String MD5(String string) {
@@ -106,11 +154,10 @@ public final class Utils {
             byte[] digest = messageDigest.digest(strBytes);
             StringBuffer sb = new StringBuffer();
             for (int i = 0; i < digest.length; ++i) {
-                sb.append(Integer.toHexString((digest[i] & 0xFF) | 0x100).substring(1,3));
+                sb.append(Integer.toHexString((digest[i] & 0xFF) | 0x100).substring(1, 3));
             }
             return sb.toString();
-        }
-        catch (java.security.NoSuchAlgorithmException e) {
+        } catch (java.security.NoSuchAlgorithmException e) {
             return string.replace("/", ".");
         }
     }
@@ -138,13 +185,16 @@ public final class Utils {
     }
 
     public static int calculateMemorySize(Context context, int percentage) {
-        ActivityManager activityManager = (ActivityManager)context.getSystemService(ACTIVITY_SERVICE);
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(ACTIVITY_SERVICE);
         int memoryClass = activityManager.getLargeMemoryClass();
         return 1024 * 1024 * memoryClass / percentage;
     }
 
     public static File getCacheFile(Context context, String identifier) {
-        return new File(context.getExternalCacheDir(), Utils.MD5(identifier));
+        File dir = context.getExternalCacheDir();
+        if (dir == null)
+            dir = context.getCacheDir();
+        return new File(dir, Utils.MD5(identifier));
     }
 
     public static byte[] toByteArray(InputStream is) throws IOException {
@@ -156,8 +206,63 @@ public final class Utils {
                 output.write(b, 0, n);
             }
             return output.toByteArray();
+        } catch (Exception e) {
+            Log.e("Utils.toByteArray", e.getMessage(), e);
+            throw new IOException(e);
         } finally {
             output.close();
         }
     }
+
+    public static void copyToFile(InputStream inStream, File file) throws IOException {
+        OutputStream outStream = new FileOutputStream(file);
+        byte[] buffer = new byte[4 * 1024];
+        int bytesRead;
+        while ((bytesRead = inStream.read(buffer)) != -1) {
+            outStream.write(buffer, 0, bytesRead);
+        }
+        Utils.close(inStream);
+        Utils.close(outStream);
+    }
+
+    public static void close(final Closeable c) {
+        if (c == null) return;
+        try {
+            c.close();
+        } catch (final Throwable ignored) {
+            // do nothing;
+        }
+    }
+
+    public static File initCacheDirectory(String prefix) {
+        File appCacheDir = MainApplication.getAppContext().getExternalCacheDir();
+        if (appCacheDir == null)
+            appCacheDir = MainApplication.getAppContext().getCacheDir();
+        // create a unique id
+        String uuid = UUID.randomUUID().toString();
+        prefix = (prefix == null || "".equals(prefix)) ? "" : prefix + "-";
+        File mCacheDir = new File(appCacheDir, prefix + uuid);
+        if (!mCacheDir.exists()) {
+            boolean success = mCacheDir.mkdirs();
+        }
+        return mCacheDir;
+    }
+
+    public static void rmDir(File dir) {
+        rmDir(dir, true);
+    }
+
+    public static void rmDir(File dir, boolean recursive) {
+        if (dir == null || !dir.isDirectory())
+            return;
+
+        for (File f : dir.listFiles()) {
+            if (recursive && f.isDirectory())
+                rmDir(f, true);
+            else if (f.isFile())
+                f.delete();
+        }
+        dir.delete();
+    }
+
 }
