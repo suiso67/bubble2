30b277236ed07b55e3b46ed07bc58b845e328e95
ed <edgar.soldin@web.de>
2022-10-19 18:14:20 +0200

    multidex support as need on older APIs

--- app/src/main/java/com/nkanaev/comics/MainApplication.java
+++ app/src/main/java/com/nkanaev/comics/MainApplication.java
@@ -3,22 +3,23 @@ package com.nkanaev.comics;
 import android.app.Application;
 import android.content.Context;
 import android.content.SharedPreferences;
+import androidx.multidex.MultiDexApplication;
 
 
-public class MainApplication extends Application {
-    private static Context mContext;
+public class MainApplication extends MultiDexApplication {
+    private static Application instance;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        mContext = this;
+        instance = this;
     }
 
     public static Context getAppContext() {
-        return mContext;
+        return instance.getApplicationContext();
     }
 
     public static SharedPreferences getPreferences() {
-        return mContext.getSharedPreferences(Constants.SETTINGS_NAME, 0);
+        return instance.getSharedPreferences(Constants.SETTINGS_NAME, 0);
     }
 }
