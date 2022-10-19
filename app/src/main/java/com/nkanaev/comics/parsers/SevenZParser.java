e0ba49bbef991fc3ae1ac8db4c897646590c3b8a
ed <edgar.soldin@web.de>
2022-10-19 17:55:13 +0200

    cache to file system, prepared to open intents

--- app/src/main/java/com/nkanaev/comics/parsers/SevenZParser.java
+++ app/src/main/java/com/nkanaev/comics/parsers/SevenZParser.java
@@ -1,72 +1,197 @@
 package com.nkanaev.comics.parsers;
 
 
+import android.content.res.AssetFileDescriptor;
+import android.net.Uri;
+import android.util.Log;
+import com.nkanaev.comics.MainApplication;
 import com.nkanaev.comics.managers.NaturalOrderComparator;
 import com.nkanaev.comics.managers.Utils;
 import org.apache.commons.compress.archivers.sevenz.SevenZArchiveEntry;
 import org.apache.commons.compress.archivers.sevenz.SevenZFile;
 
 import java.io.*;
+import java.nio.channels.FileChannel;
+import java.nio.channels.SeekableByteChannel;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class SevenZParser implements Parser {
-    private List<SevenZEntry> mEntries;
+public class SevenZParser extends AbstractParser {
+    private SevenZFile mSevenZFile = null;
+    private List<SevenZArchiveEntry> mEntries = null;
+    private File mCacheDir = null;
+    private boolean mCachePopulated = false;
 
-    private class SevenZEntry {
-        final SevenZArchiveEntry entry;
-        final byte[] bytes;
-
-        public SevenZEntry(SevenZArchiveEntry entry, byte[] bytes) {
-            this.entry = entry;
-            this.bytes = bytes;
-        }
+    public SevenZParser() {
+        super(new Class[]{File.class});
     }
 
     @Override
-    public void parse(File file) throws IOException {
-        mEntries = new ArrayList<>();
-        SevenZFile sevenZFile = new SevenZFile(file);
+    public void parse() throws IOException {
+        Object source = getSource();
+        if (source instanceof File)
+            parse((File) source);
+        else
+            throw new UnsupportedOperationException();
+    }
 
-        SevenZArchiveEntry entry = sevenZFile.getNextEntry();
-        while (entry != null) {
+    protected void parse(File file) throws IOException {
+        mSevenZFile = new SevenZFile(file);
+        parse(mSevenZFile);
+    }
+
+    protected void parse(SevenZFile sevenZFile) throws IOException {
+        if (mSevenZFile != null && mEntries != null)
+            return;
+
+        mSevenZFile = sevenZFile;
+        mEntries = new ArrayList<>();
+
+        for (SevenZArchiveEntry entry : sevenZFile.getEntries()) {
             if (entry.isDirectory()) {
                 continue;
             }
             if (Utils.isImage(entry.getName())) {
-                byte[] content = new byte[(int)entry.getSize()];
-                sevenZFile.read(content);
-                mEntries.add(new SevenZEntry(entry, content));
+                mEntries.add(entry);
             }
-            entry = sevenZFile.getNextEntry();
         }
 
         Collections.sort(mEntries, new NaturalOrderComparator() {
             @Override
             public String stringValue(Object o) {
-                return ((SevenZEntry) o).entry.getName();
+                return ((SevenZArchiveEntry) o).getName();
             }
         });
     }
 
     @Override
-    public int numPages() {
+    public int numPages() throws IOException {
+        parse();
         return mEntries.size();
+    }
+
+    private SevenZFile from(Uri uri) throws IOException {
+        AssetFileDescriptor afd = MainApplication.getAppContext().getContentResolver().openAssetFileDescriptor(uri, "r");
+        FileChannel fc = new FileInputStream(afd.getFileDescriptor()).getChannel();
+        return from(fc);
+    }
+
+    private SevenZFile from(SeekableByteChannel channel) throws IOException {
+        mSevenZFile = new SevenZFile(channel);
+        return mSevenZFile;
+    }
+
+    private SevenZFile from(File file) throws IOException {
+        mSevenZFile = new SevenZFile(file);
+        return mSevenZFile;
     }
 
     @Override
     public InputStream getPage(int num) throws IOException {
-        return new ByteArrayInputStream(mEntries.get(num).bytes);
+        try {
+            // lazy parse
+            parse();
+
+            SevenZArchiveEntry needle = mEntries.get(num);
+            // repeated reads throw "org.tukaani.xz.CorruptedInputException: Compressed data is corrupt"
+            //return mSevenZFile.getInputStream(entry);
+            // as a workaround we reopen and read each entry by tself
+            //return getInputStream(entry);
+            // or not, too slow by far, so extract'n'cache
+
+            // populate cache (cover excluded)
+            synchronized (this) {
+                if (true && !mCachePopulated && num != 0) {
+                    SevenZFile sevenZFile;
+                    Object source = getSource();
+                    if (source instanceof Uri)
+                        sevenZFile = from((Uri) source);
+                    else
+                        sevenZFile = from((File) source);
+
+                    initCacheDirectory(MainApplication.getAppContext().getExternalCacheDir());
+                    // populate file cache
+                    SevenZArchiveEntry entry;
+                    while ((entry = sevenZFile.getNextEntry()) != null) {
+                        if (Utils.isImage(entry.getName())) {
+                            File cacheFile = new File(mCacheDir, Utils.MD5(entry.getName()));
+                            //byte[] content = new byte[(int) entry.getSize()];
+                            //sevenZFile.read(content);
+                            //Utils.copyToFile(new ByteArrayInputStream(content), cacheFile);
+                            Utils.copyToFile(sevenZFile.getInputStream(entry),cacheFile);
+                        }
+                    }
+                    mCachePopulated = true;
+                    Utils.close(sevenZFile);
+                }
+            }
+
+            // try cache
+            if (mCacheDir != null && mCachePopulated) {
+                String name = needle.getName();
+                File cacheFile = new File(mCacheDir, Utils.MD5(name));
+
+                if (cacheFile.exists()) {
+                    return new FileInputStream(cacheFile);
+                }
+            }
+
+            // last chance, just read thru archive (very slow for file towards the end)
+            return getInputStream(needle);
+        } catch (Exception e) {
+            Log.e("SevebZParser", "getPage", e);
+            throw new IOException(e);
+        }
+    }
+
+    private InputStream getInputStream(SevenZArchiveEntry needle) throws IOException {
+        synchronized (this) {
+            SevenZFile sevenZFile;
+            Object source = getSource();
+            if (source instanceof Uri)
+                sevenZFile = from((Uri) source);
+            else
+                sevenZFile = from((File) source);
+
+            try {
+                // slow need to populate file cache
+                SevenZArchiveEntry entry;
+                while ((entry = sevenZFile.getNextEntry()) != null) {
+                    if (entry.getName().equals(needle.getName())) {
+                        byte[] content = new byte[(int) entry.getSize()];
+                        sevenZFile.read(content);
+                        return new ByteArrayInputStream(content);
+                    }
+                }
+            } finally {
+                Utils.close(sevenZFile);
+            }
+        }
+        throw new IOException(needle.getName() + " not found in archive.");
     }
 
     @Override
     public String getType() {
-        return "tar";
+        return "7z";
     }
 
     @Override
-    public void destroy() throws IOException {
+    public void destroy() {
+        Utils.close(mSevenZFile);
+        mSevenZFile = null;
+        mEntries = null;
+        if (mCacheDir != null) {
+            for (File f : mCacheDir.listFiles()) {
+                f.delete();
+            }
+            mCacheDir.delete();
+        }
+        mCachePopulated = false;
+        mCacheDir = null;
+    }
 
+    private void initCacheDirectory(File cacheDirectory) {
+        mCacheDir = Utils.initCacheDirectory("sev");
     }
 }
