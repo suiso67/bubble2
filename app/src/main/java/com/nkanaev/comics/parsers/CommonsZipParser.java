e185068692d3afbf01c256b0b4c69f6a20b7c093
ed <edgar.soldin@web.de>
2022-10-19 20:50:33 +0200

    new zip parser based on apache commons
    should support a broader range of zip files*
    *untested

---
+++ app/src/main/java/com/nkanaev/comics/parsers/CommonsZipParser.java
@@ -0,0 +1,77 @@
+package com.nkanaev.comics.parsers;
+
+import com.nkanaev.comics.managers.NaturalOrderComparator;
+import com.nkanaev.comics.managers.Utils;
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
+import org.apache.commons.compress.archivers.zip.ZipFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+
+
+public class CommonsZipParser extends AbstractParser {
+    private ZipFile mZipFile = null;
+    private ArrayList<ZipArchiveEntry> mEntries = null;
+
+    public CommonsZipParser() {
+        super(new Class[]{File.class});
+    }
+
+    @Override
+    public void parse() throws IOException {
+        if (mZipFile != null && mEntries != null)
+            return;
+
+        File file = (File) getSource();
+
+        mZipFile = new ZipFile(file.getAbsolutePath());
+        mEntries = new ArrayList<>();
+
+        Enumeration<ZipArchiveEntry> e = mZipFile.getEntries();
+        while (e.hasMoreElements()) {
+            ZipArchiveEntry ze = e.nextElement();
+            if (!ze.isDirectory() && Utils.isImage(ze.getName())) {
+                mEntries.add(ze);
+            }
+        }
+
+        //noinspection unchecked
+        Collections.sort(mEntries,
+                new NaturalOrderComparator() {
+                    @Override
+                    public String stringValue(Object o) {
+                        return ((ZipArchiveEntry) o).getName();
+                    }
+                });
+    }
+
+    @Override
+    public int numPages() throws IOException {
+        // lazy parse
+        parse();
+        return mEntries.size();
+    }
+
+    @Override
+    public InputStream getPage(int num) throws IOException {
+        parse();
+        return mZipFile.getInputStream(mEntries.get(num));
+    }
+
+    @Override
+    public String getType() {
+        return "zip";
+    }
+
+    @Override
+    public void destroy() {
+        Utils.close(mZipFile);
+        mZipFile = null;
+        mEntries = null;
+    }
+
+}
