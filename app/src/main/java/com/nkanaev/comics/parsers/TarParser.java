e860227bcbc55fdba9825d93c004ea4608f89e6f
ed <edgar.soldin@web.de>
2022-10-19 17:51:52 +0200

    add several Compressions
    NOTE: GZ/LZMA are veeerrrrrrrryyyy slow

--- app/src/main/java/com/nkanaev/comics/parsers/TarParser.java
+++ app/src/main/java/com/nkanaev/comics/parsers/TarParser.java
@@ -5,18 +5,22 @@ import com.nkanaev.comics.managers.NaturalOrderComparator;
 import com.nkanaev.comics.managers.Utils;
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.compress.compressors.CompressorException;
+import org.apache.commons.compress.compressors.CompressorStreamFactory;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class TarParser implements Parser {
-    private List<TarEntry> mEntries;
+public class TarParser extends AbstractParser {
+    private List<TarEntry> mEntries = null;
+
+    private boolean mParsedAlready = false;
 
     private class TarEntry {
         final TarArchiveEntry entry;
-        final byte[] bytes;
+        byte[] bytes;
 
         public TarEntry(TarArchiveEntry entry, byte[] bytes) {
             this.entry = entry;
@@ -24,38 +28,90 @@ public class TarParser implements Parser {
         }
     }
 
-    @Override
-    public void parse(File file) throws IOException {
-        mEntries = new ArrayList<>();
+    public TarParser() {
+        super(new Class[]{File.class});
+    }
 
-        BufferedInputStream fis = new BufferedInputStream(new FileInputStream(file));
-        TarArchiveInputStream is = new TarArchiveInputStream(fis);
-        TarArchiveEntry entry = is.getNextTarEntry();
-        while (entry != null) {
-            if (entry.isDirectory()) {
-                continue;
-            }
-            if (Utils.isImage(entry.getName())) {
-                mEntries.add(new TarEntry(entry, Utils.toByteArray(is)));
-            }
-            entry = is.getNextTarEntry();
+    private TarArchiveInputStream initStream(File file) throws IOException {
+        String fileName = file.getName();
+        InputStream is = null;
+
+        is = new BufferedInputStream(new FileInputStream(file));
+        try {
+            if (Utils.isTGZ(fileName))
+                is = new CompressorStreamFactory().createCompressorInputStream(CompressorStreamFactory.GZIP, is);
+            else if (Utils.isTBZ(fileName))
+                is = new CompressorStreamFactory().createCompressorInputStream(CompressorStreamFactory.BZIP2, is);
+            else if (Utils.isTXZ(fileName))
+                is = new CompressorStreamFactory().createCompressorInputStream(CompressorStreamFactory.LZMA, is);
+            else if (Utils.isTZST(fileName))
+                is = new CompressorStreamFactory().createCompressorInputStream(CompressorStreamFactory.ZSTANDARD, is);
+            else if (Utils.isTBR(fileName))
+                is = new CompressorStreamFactory().createCompressorInputStream(CompressorStreamFactory.BROTLI, is);
+        } catch (CompressorException e) {
+            throw new IOException(e);
         }
 
-        Collections.sort(mEntries, new NaturalOrderComparator() {
-            @Override
-            public String stringValue(Object o) {
-                return ((TarEntry) o).entry.getName();
+        return new TarArchiveInputStream(is, true);
+    }
+
+    public void parse() throws IOException {
+        parse(1);
+    }
+
+    private void parse(int limit) throws IOException {
+        if (mParsedAlready)
+            return;
+
+        File file = (File) getSource();
+
+        TarArchiveInputStream tis = null;
+        try {
+            tis = initStream(file);
+            TarArchiveEntry entry;
+            ArrayList entries = new ArrayList<>();
+            while ((entry = tis.getNextTarEntry()) != null) {
+                if (entry.isDirectory()) {
+                    continue;
+                }
+                // TODO: cache to file system instead
+                if (Utils.isImage(entry.getName())) {
+                    boolean read = limit < 0 || entries.size() <= limit;
+                    entries.add(new TarEntry(entry, read ? Utils.toByteArray(tis) : null));
+                }
             }
-        });
+
+            Collections.sort(entries, new NaturalOrderComparator() {
+                @Override
+                public String stringValue(Object o) {
+                    return ((TarEntry) o).entry.getName();
+                }
+            });
+
+            mEntries = entries;
+            mParsedAlready = true;
+        } finally {
+            Utils.close(tis);
+        }
+
     }
 
     @Override
-    public int numPages() {
-        return mEntries.size();
+    public int numPages() throws IOException {
+        parse(0);
+        return mEntries != null ? mEntries.size() : 0;
     }
 
     @Override
-    public InputStream getPage(int num) throws IOException {
+    public synchronized InputStream getPage(int num) throws IOException {
+        synchronized (mEntries) {
+            byte[] data = mEntries.get(num).bytes;
+            // (re)read archive data to memory
+            if (data == null) {
+                mParsedAlready = false;
+                parse(-1);
+            }
+        }
         return new ByteArrayInputStream(mEntries.get(num).bytes);
     }
 
@@ -65,7 +121,12 @@ public class TarParser implements Parser {
     }
 
     @Override
-    public void destroy() throws IOException {
-
+    public void destroy() {
+        // delete all cached but cover page (0)
+        for (int i = 1; i < mEntries.size(); i++) {
+            mEntries.get(i).bytes = null;
+        }
+        mParsedAlready = false;
     }
+
 }
