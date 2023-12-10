3b3cdd5cc8dd3e9904b7b2ef0461bdc92de581c1
ed <edgar.soldin@web.de>
2023-12-10 21:01:37 +0100

    robustize image load by inSampleSizing
    display up to 30k times 30k pixels

--- app/src/main/java/com/nkanaev/comics/managers/LocalComicHandler.java
+++ app/src/main/java/com/nkanaev/comics/managers/LocalComicHandler.java
@@ -1,11 +1,13 @@
 package com.nkanaev.comics.managers;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.util.Log;
 import com.nkanaev.comics.parsers.*;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Request;
 import com.squareup.picasso.RequestHandler;
-import okio.Okio;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -28,7 +30,51 @@ public class LocalComicHandler extends RequestHandler {
     public Result load(Request request, int networkPolicy) throws IOException {
         int pageNum = Integer.parseInt(request.uri.getFragment());
         InputStream stream = mParser.getPage(pageNum);
-        return new Result(Okio.source(stream), Picasso.LoadedFrom.DISK);
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(stream, null, options);
+        Utils.close(stream);
+        int w = options.outWidth;
+        int h = options.outHeight;
+        String id = w+"x"+h;
+        int maxMemorySize = Utils.bitmapMaxMemorySize();
+
+        options = new BitmapFactory.Options();
+        // dying on mangastrips with GL error, max texture size?
+        if (false && Utils.isOreoOrLater() && Math.max(w,h) <= Utils.glMaxTextureSize())
+            options.inPreferredConfig = Bitmap.Config.HARDWARE;
+        // assuming default 4byte Bitmap.Config.ARGB_8888
+        if (maxMemorySize > 0 && (long)w * h * 4 > maxMemorySize) {
+            // check if resample is needed assuming 4byte Bitmap.Config.ARGB_4444
+            int inSampleSize = 1;
+            while ((long)w * h * 4 > maxMemorySize) {
+                inSampleSize *= 2;
+                h /= 2;
+                w /= 2;
+            }
+            options.inSampleSize = inSampleSize;
+            Log.d("inSampleSize "+id, String.valueOf(inSampleSize));
+        }
+
+        stream = mParser.getPage(pageNum);
+        Bitmap result = BitmapFactory.decodeStream(stream, null, options);
+        if (Utils.isKitKatOrLater()) {
+            int m = result.getAllocationByteCount();
+            Log.d("alloc " + id, String.valueOf(m));
+        }
+        Log.d("config "+id, result.getConfig().toString());
+/*
+        Bitmap oldResult = result;
+        // make extra sure result ist 565
+        if (false && result != null) {
+            result = result.copy(Bitmap.Config.RGB_565, false);
+            Utils.close(oldResult);
+        }
+*/
+        Utils.close(stream);
+        return new Result(result, Picasso.LoadedFrom.DISK);
+        //return new Result(Okio.source(stream), Picasso.LoadedFrom.DISK);
     }
 
 /*  // trial to adapt to picasso 3.0.0-alpha
