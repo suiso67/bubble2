c191a0e7059f42786148834f53edc1181d302f23
ed <edgar.soldin@web.de>
2025-01-26 20:24:55 +0100

    display dependency's maven definition

--- app/src/main/java/com/nkanaev/comics/fragment/AboutFragment.java
+++ app/src/main/java/com/nkanaev/comics/fragment/AboutFragment.java
@@ -16,9 +16,14 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import androidx.fragment.app.Fragment;
+import com.nkanaev.comics.BuildConfig;
 import com.nkanaev.comics.R;
 import com.nkanaev.comics.activity.MainActivity;
 import net.sf.sevenzipjbinding.SevenZip;
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AboutFragment extends Fragment implements View.OnClickListener {
     static private class LibraryDescription {
@@ -41,73 +46,114 @@ public class AboutFragment extends Fragment implements View.OnClickListener {
         SevenZip.Version version = SevenZip.getSevenZipVersion();
         CharSequence out =
                 "7-zip version: " + version.version + ", " + version.date + "\n" +
-                "7-Zip-JBinding version: " + SevenZip.getSevenZipJBindingVersion() + "\n" +
-                "Native library initialized: " + SevenZip.isInitializedSuccessfully();
-        final SpannableString s = new SpannableString(out);
-        s.setSpan(new AlignmentSpan.Standard(Layout.Alignment.ALIGN_OPPOSITE),0,s.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                        "7-Zip-JBinding version: " + SevenZip.getSevenZipJBindingVersion() + "\n" +
+                        "Native library initialized: " + SevenZip.isInitializedSuccessfully();
+        return alignRight(out);
+    }
+
+    private static CharSequence alignRight(CharSequence text) {
+        final SpannableString s = new SpannableString(text);
+        s.setSpan(new AlignmentSpan.Standard(Layout.Alignment.ALIGN_OPPOSITE), 0, s.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         return s;
+    }
+
+    private static List<String> dependencyList = null;
+
+    private static CharSequence getDependencyEntries(String... needles) {
+        // lazy init
+        if (dependencyList == null) {
+            dependencyList = new ArrayList();
+            try {
+                JSONArray jsonarray = new JSONArray(BuildConfig.DEPENDENCIES);
+                for (int i = 0; i < jsonarray.length(); i++) {
+                    dependencyList.add(jsonarray.getString(i));
+                }
+            } catch (Exception e) {
+                Log.e("AboutFragment#182", "getVersionString()", e);
+            }
+        }
+
+        // find entries
+        String out = "";
+        for (String depString : dependencyList) {
+            for (String needle : needles) {
+                if (depString.toLowerCase().contains(needle.toLowerCase()))
+                    out += (out.isEmpty() ? "" : "\n") + depString;
+            }
+        }
+        return out.isEmpty() ? "" : TextUtils.concat("\n\n", alignRight(out));
     }
 
     private LibraryDescription[] mDescriptions = new LibraryDescription[]{
             new LibraryDescription(
                     "Picasso",
-                    "A powerful image downloading and caching library for Android",
+                    TextUtils.concat("A powerful image downloading and caching library for Android",
+                            getDependencyEntries("Picasso")),
                     "Apache Version 2.0",
                     "Square",
                     "https://github.com/square/picasso"
             ),
             new LibraryDescription(
                     "Junrar",
-                    "Plain java unrar util",
+                    TextUtils.concat("Plain java unrar util",
+                            getDependencyEntries("Junrar")),
                     "Unrar license",
                     "Edmund Wagner",
                     "https://github.com/edmund-wagner/junrar"
             ),
             new LibraryDescription(
                     "Apache Commons Compress",
-                    "Defines an API for working with tar, zip and bzip2 files",
+                    TextUtils.concat("Defines an API for working with tar, zip and bzip2 files",
+                            getDependencyEntries("commons-compress")),
                     "Apache Version 2.0",
                     "Apache Software Foundation",
                     "https://commons.apache.org/proper/commons-compress/"
             ),
             new LibraryDescription(
                     "XZ Utils",
-                    "XZ Utils is free general-purpose data compression software with a high compression ratio",
+                    TextUtils.concat("XZ Utils is free general-purpose data compression software with a high compression ratio",
+                            getDependencyEntries("tukaani")),
                     "Public Domain",
                     "Tukaani Developers",
                     "http://tukaani.org/xz/java.html"
             ),
             new LibraryDescription(
                     "7-Zip-JBinding-4Android",
-                    TextUtils.concat("Android library version of 7-Zip-JBinding java wrapper.\n\n", lib7zDetails()),
+                    TextUtils.concat("Android library version of 7-Zip-JBinding java wrapper.\n\n",
+                            lib7zDetails(),
+                            getDependencyEntries("7-Zip-JBinding-4Android")),
                     "GNU LGPL 2.1 or later + unRAR restriction",
                     "Igor Pavlov, Boris Brodski, Fredrik Claesson",
                     "https://github.com/omicronapps/7-Zip-JBinding-4Android"
             ),
             new LibraryDescription(
                     "Zstd-jni",
-                    "JNI bindings to Zstd Library",
+                    TextUtils.concat("JNI bindings to Zstd Library",
+                            getDependencyEntries("zstd")),
                     "BSD license",
                     "Luben Karavelov",
                     "https://github.com/luben/zstd-jni"
             ),
             new LibraryDescription(
                     "Brotli",
-                    "Generic-purpose lossless compression algorithm",
+                    TextUtils.concat("Generic-purpose lossless compression algorithm",
+                            getDependencyEntries("brotli")),
                     "MIT license",
                     "Google",
                     "https://github.com/google/brotli"
             ),
             new LibraryDescription(
                     "JP2 for Android",
-                    "Open-source JPEG-2000 image encoder/decoder for Android based on OpenJPEG",
+                    TextUtils.concat("Open-source JPEG-2000 image encoder/decoder for Android based on OpenJPEG",
+                            getDependencyEntries("jp2-android")),
                     "BSD (2-clause) license",
-                    "ThalesGroup",
-                    "https://github.com/ThalesGroup/JP2ForAndroid"
+                    "ThalesGroup, Keiji",
+                    "https://github.com/keiji/JP2ForAndroid"
             ),
             new LibraryDescription(
                     "Natural Sorting for Java",
-                    "Perform 'natural order' comparisons of strings in Java.",
+                    TextUtils.concat("Perform 'natural order' comparisons of strings in Java.",
+                            alignRight("\n\nv1.2.0")),
                     "MIT license",
                     "Jagoba Gascón",
                     "https://github.com/jagobagascon/Natural-Sorting-for-Java"
@@ -121,7 +167,8 @@ public class AboutFragment extends Fragment implements View.OnClickListener {
             ),
             new LibraryDescription(
                     "Android Jetpack",
-                    "Androidx suite of tools and libraries",
+                    TextUtils.concat("Androidx suite of tools and libraries",
+                            getDependencyEntries("androidx", "material")),
                     "Apache Version 2.0",
                     "Google",
                     "https://developer.android.com/jetpack"
@@ -133,7 +180,7 @@ public class AboutFragment extends Fragment implements View.OnClickListener {
         final View view = inflater.inflate(R.layout.fragment_about, container, false);
 
         TextView aboutVersionHeaderTextView = view.findViewById(R.id.aboutVersionHeader);
-        aboutVersionHeaderTextView.setText(aboutVersionHeaderTextView.getText()+": ");
+        aboutVersionHeaderTextView.setText(aboutVersionHeaderTextView.getText() + ": ");
 
         TextView aboutVersionTextView = view.findViewById(R.id.aboutVersion);
         aboutVersionTextView.setText(getVersionString());
@@ -142,7 +189,7 @@ public class AboutFragment extends Fragment implements View.OnClickListener {
         View appLayout = view.findViewById(R.id.about_application);
         appLayout.setTag(getString(R.string.app_link));
         TextView descView = view.findViewById(R.id.aboutDescription);
-        descView.setText(" - "+descView.getText());
+        descView.setText(" - " + descView.getText());
         appLayout.setOnClickListener(this);
 
         LinearLayout libsLayout = (LinearLayout) view.findViewById(R.id.about_libraries);
@@ -160,7 +207,7 @@ public class AboutFragment extends Fragment implements View.OnClickListener {
         }
 
         getActivity().setTitle(R.string.menu_about);
-        ((MainActivity)getActivity()).setSubTitle("");
+        ((MainActivity) getActivity()).setSubTitle("");
         return view;
     }
 
@@ -171,7 +218,7 @@ public class AboutFragment extends Fragment implements View.OnClickListener {
                     .getPackageInfo(getActivity().getPackageName(), 0);
             return pi.versionName + " (" + Integer.toString(pi.versionCode) + ")";
         } catch (Exception e) {
-            Log.e("AboutFragment#117","getVersionString()",e);
+            Log.e("AboutFragment#195", "getVersionString()", e);
             return "";
         }
     }
