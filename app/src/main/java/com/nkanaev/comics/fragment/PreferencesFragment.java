2c93ea8cd393ef90b60dc575838276737952ad4a
suiso67 <suiso67@macadamia.rocks>
2025-02-06 11:59:42 +0900

    feat: add title and summary to preferences screen

--- app/src/main/java/com/nkanaev/comics/fragment/PreferencesFragment.java
+++ app/src/main/java/com/nkanaev/comics/fragment/PreferencesFragment.java
@@ -8,6 +8,13 @@ import com.nkanaev.comics.R;
 
 public class PreferencesFragment extends PreferenceFragmentCompat {
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getActivity().setTitle(R.string.menu_preferences);
+    }
+
+    @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
     }
